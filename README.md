a767a108f3349b3d279a000ce41da7fb8c23385c
mamemomonga <mamemomonga@gmail.com>
2018-08-27 02:57:05 +0900

    README更新

--- README.md
+++ README.md
@@ -3,4 +3,28 @@
 * Raspberry Pi3
 * Docker
 * /var を btrfsの外付けSSD
+* ./build.sh を実行すればビルド開始
+* DO\_PUSH=1 を頭につけるとPUSHする:w
+* postgres:9.6-alpine, redis:4.0-alpine は ARM対応
+
+# Raspberry PiへのDockerインストール
+
+	$ curl https://get.docker.com/ | sh
+	$ sudo sh -c 'usermod -a -G docker $SUDO_USER'
+	
+	再ログイン
+
+# Docker Repository
+
+* https://hub.docker.com/r/mamemomonga/multiarch-armhf-ruby/
+* https://hub.docker.com/r/mamemomonga/multiarch-armhf-mastodon/
+
+# 参考URL
+
+* https://github.com/docker-library/ruby/tree/c43fef8a60cea31eb9e7d960a076d633cb62ba8d/2.4/alpine3.6/
+* https://hub.docker.com/r/multiarch/alpine/tags/
+* https://github.com/tootsuite/mastodon
+* https://github.com/tootsuite/documentation/blob/master/Running-Mastodon/Production-guide.md
+* https://hub.docker.com/\_/postgres/
+* https://hub.docker.com/\_/redis/
 
--- build.sh
+++ build.sh
@@ -6,16 +6,12 @@ if [ -d build ]; then echo "build directory already exists"; exit 1; fi
 mkdir build
 cd build
 
-# https://github.com/docker-library/ruby/tree/c43fef8a60cea31eb9e7d960a076d633cb62ba8d/2.4/alpine3.6/
-# https://hub.docker.com/r/multiarch/alpine/tags/
 mkdir ruby
 cd ruby
 wget https://raw.githubusercontent.com/docker-library/ruby/c43fef8a60cea31eb9e7d960a076d633cb62ba8d/2.4/alpine3.6/Dockerfile
 perl -i -nlpE 's#\QFROM alpine:3.6\E#FROM multiarch/alpine:armhf-v3.6#' Dockerfile
 cd ..
  
-# https://github.com/tootsuite/mastodon
-# https://github.com/tootsuite/documentation/blob/master/Running-Mastodon/Production-guide.md
 git clone https://github.com/tootsuite/mastodon.git
 cd mastodon
 export MASTODON_VER=$(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
@@ -31,7 +27,4 @@ if [ -n "${DO_PUSH:-}" ]; then
 	docker push mamemomonga/multiarch-armhf-mastodon:$MASTODON_VER
 fi
 
-# postgres:9.6-alpine, redis:4.0-alpine は ARM対応
-# https://hub.docker.com/_/postgres/
-# https://hub.docker.com/_/redis/
 
