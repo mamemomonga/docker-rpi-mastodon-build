f81e01e3dc29cc394ac8ab7bd3bae8744a57e31c
mamemomonga <mamemomonga@gmail.com>
2018-08-28 18:32:42 +0900

    v2.5.1対応他

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eu
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $BASEDIR
 
-if [ -e 'var' ]; then echo "var directory already exists."; exit 1; fi
+# if [ -e 'var' ]; then echo "var directory already exists."; exit 1; fi
 
 if [ -z "$( uname -m | perl -nlE 'say if (/^armv(6|7)l$/)' )" ]; then
 	echo "This tool can only be used with armv6l or armv7l."
@@ -57,24 +57,33 @@ function build_docker-compose {
 }
 
 function build_mastodon {
-	# Stable
-	# local mastodon_ver=$( git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1 )
-	# RC
-	# local mastodon_ver=$( git tag -l | grep 'rc[0-9]*$' | sort -V | tail -n 1 )
-	local tagname="mamemomonga/multiarch-armhf-mastodon:$mastodon_ver"
-	echo "*** BUILD $tagname ***"
 	mkdir -p var
 	cd var
-	git clone https://github.com/tootsuite/mastodon.git
-	cd mastodon
+	set -x
+
+	if [ -e mastodon ]; then
+		cd mastodon
+		git reset --hard
+	else
+		git clone https://github.com/tootsuite/mastodon.git
+	fi
+
+	# RC
+	# local mastodon_ver=$( git tag -l | grep 'rc[0-9]*$' | sort -V | tail -n 1 )
+
+	# Stable
+	local mastodon_ver=$( git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1 )
+
+	local tagname="mamemomonga/multiarch-armhf-mastodon:$mastodon_ver"
+	echo "*** BUILD $tagname ***"
+
 	git checkout $mastodon_ver
 
-	exit
-
 	# patch
-	patch -p1 < ../../patch/mastodon-v2.4.5-arm.patch
+	patch -p1 < '../../patch/mastodon-'$mastodon_ver'-arm.patch'
 
 	run_build $tagname
+	set +x
 }
 
 function run_build {
